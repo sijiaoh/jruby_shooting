c0e7204c680017526d57ca97ed602bf18e3ebece
Sijia Wang <sijiaoh@outlook.com>
2022-02-10 16:54:23 +0900

    Change component methods to real method.

--- src/core/game_object/component.rb
+++ src/core/game_object/component.rb
@@ -3,11 +3,7 @@ require "./src/core/game_object/game_object"
 class Component
   attr_accessor :game_object
 
-  def method_missing(name, *args, &block)
-    super(name, *args, &block) if GameObject::COMPONENT_LIFECYCLES.exclude? name.to_sym
-  end
-
-  def respond_to_missing?(sym, _include_private)
-    GameObject::COMPONENT_LIFECYCLES.include? sym
+  GameObject::COMPONENT_LIFECYCLES.each do |method|
+    define_method(method) {}
   end
 end
